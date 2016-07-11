348d74a059b1ad8ed6f3d4d935bb4144b7db7070
Korirmitchelle <korirmitchelle@gmail.com>
2016-07-11 10:46:03 +0300

    animate

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-$(document).foundation()
+$(function () {
+            function count($this) {
+                var current = parseInt($this.html(), 10);
+                current = current + 50;
+                /* Where
+50 is increment */
+                $this.html(++current);
+                if (current > $this.data('count')) {
+                    $this.html($this.data('count'));
+                } else {
+                    setTimeout(function () {
+                        count($ this)
+                    }, 5);
+                }
+            }
+            $(".stat-count").each(function () {
+                $(this).data('count', parseInt($(this)
+                    .html(), 10));
+                $(this).html('0');
+                count($(this));
+            });
